aed627d5b6915d7dde8ff48639109a93c9e59373
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-20 19:06:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def TimeFormatter(milliseconds: int) -> str:
 
 
 @HB.on_message(filters.regex(VIDEO_REGEX))
-async def ytdl(_, message, update):
+async def ytdl(_, message):
    l = message.text.split()
    global var
    global ythd
