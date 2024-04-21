86821f17c7410d25107c4fcbbfbc1f788f67b395
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-21 18:27:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ async def ytdl(_, message):
     var = message.text
     global url
     url = message.text
-    yt = YouTube(url)
+    yt = YouTube(url, use_oauth=False, allow_oauth_cache=True)
     chat_id = message.chat.id
     thumb = yt.thumbnail_url
     length = yt.length
